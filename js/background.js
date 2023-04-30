85a03fc94966a8e18a50cc6ddb6e03065c03bf30
장정안 <wjddks96@naver.com>
2023-04-30 19:45:17 +0900

    nomad Challenge Fianl

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
-
+const body = document.getElementById('body');
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
 const bgImage = document.createElement("img");
 document.body.appendChild(bgImage);
 bgImage.src = `img/${chosenImage}`;
+
+bgImage.style.width = '100%';
+bgImage.style.height = '100%';
